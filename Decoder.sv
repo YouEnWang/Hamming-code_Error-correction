1787c4e5562f6e91c08bbcdbd88b0a18472f771f
YouEnWang <113414093+YouEnWang@users.noreply.github.com>
2023-07-12 23:10:37 +0800

    Update Decoder.sv

--- Decoder.sv
+++ Decoder.sv
@@ -1,31 +1,32 @@
 // Decoder
+// main design code
 module Decoder(data_in, data_out, do_XOR, M);
   input [31:0] data_in;
   output [31:0] data_out;
-  output [4:0] do_XOR;			
-  output [4:0] M;
+  output [5:0] do_XOR;			// 用6位
+  output [5:0] M;
     
   reg [31:0] data_out = 32'd0;
-  reg [4:0] place_of_1 [31:0];
-  reg [4:0] counter = 5'd0;		// 計數用
-  reg [4:0] i = 5'd0;		// 計數用
-  reg [4:0] j = 5'd0;		// 計數用
-  reg [4:0] M = 5'd0;
-  reg [4:0] do_XOR = 5'd0;
+  reg [5:0] place_of_1 [31:0];
+  reg [5:0] counter = 6'd0;		// 計數用
+  reg [5:0] i = 6'd0;		// 計數用
+  reg [5:0] j = 6'd0;		// 計數用
+  reg [5:0] M = 6'd0;
+  reg [5:0] do_XOR = 6'd0;
 
   initial begin
     // 計算M: input有幾個bit
     // 把input先放進output內
-    while(data_in > 2**M) begin
+    while(i < 2**M) begin
       data_out[M] = data_in[M];
-      M = M + 1;
+      M = M + 6'd1;
     end
     
     // 將output內目前所有為1的位置放進place_of_1
     // 存入的值會是二進位表示
     while(i < M) begin
       if(data_in[M-i-1] == 1) begin
-        place_of_1[counter] = i+1;
+        place_of_1[counter] = i;
         counter = counter + 1;
       end
       i = i + 1;
@@ -36,10 +37,12 @@ module Decoder(data_in, data_out, do_XOR, M);
       do_XOR = do_XOR ^ place_of_1[j];
       j = j + 1;
     end
-	
+    
+	do_XOR = do_XOR + 6'd1;
+    
     // 若do_XOR非0，則表示有error
     // 將error bit做invertor
-    if(do_XOR != 5'd0) begin
+    if(do_XOR != 6'd0) begin
       data_out[M - do_XOR] = ~data_out[M - do_XOR];
     end
   end
